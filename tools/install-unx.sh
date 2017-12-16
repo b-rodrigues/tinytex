c34fb4cad1041c46399d3280050691dd5623cfd1
Bruno Rodrigues <brodriguesco@protonmail.com>
2017-12-17 00:03:43 +0100

    Update install-unx.sh
    updated links

--- tools/install-unx.sh
+++ tools/install-unx.sh
@@ -12,8 +12,8 @@ else
   alias download2='wget -q'
 fi
 
-download2 https://github.com/yihui/tinytex/raw/master/tools/texlive.profile
-download  https://github.com/yihui/tinytex/raw/master/tools/install-base.sh | sh -s - "$@"
+download2 https://github.com/b-rodrigues/tinytex/raw/master/tools/texlive.profile
+download  https://github.com/b-rodrigues/tinytex/raw/master/tools/install-base.sh | sh -s - "$@"
 rm texlive.profile
 
 rm -rf $TEXDIR
@@ -21,7 +21,7 @@ mkdir -p $TEXDIR
 mv texlive/* $TEXDIR
 rm -r texlive
 
-$TEXDIR/bin/*/tlmgr install $(download https://github.com/yihui/tinytex/raw/master/tools/pkgs-custom.txt | tr '\n' ' ')
+$TEXDIR/bin/*/tlmgr install $(download https://github.com/b-rodrigues/tinytex/raw/master/tools/pkgs-custom.txt | tr '\n' ' ')
 
 if [ "$1" = '--admin' ]; then
   if [ "$2" != '--no-path' ]; then
